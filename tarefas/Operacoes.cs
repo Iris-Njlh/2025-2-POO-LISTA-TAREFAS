f1586a3727bb43a16737c7a742eb05355d674987
Iris-Njlh <iris.lima@uni9.edu.br>
2025-10-20 23:19:23 +0000

    tarefas

--- tarefas/Operacoes.cs
+++ tarefas/Operacoes.cs
@@ -1,6 +1,8 @@
+using MySql.Data.MySqlClient;
+
 public class Operacoes
 {
-    private string connectionString = "";
+    private string connectionString = @"server=phpmyadmin.uni9.marize.us;User ID=user_poo;password=S3nh4!F0rt3;database=user_poo;";
     public int Criar(Tarefa tarefa)
     {
         using(var conexao = new MySqlConnection(connectionString))
